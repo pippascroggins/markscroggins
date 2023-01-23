ea046dd2dfdba817e3a169ca0f86de58ecae3164
Philippa Scroggins <prscroggins@wm.edu>
2023-01-23 12:45:22 -0500

    Add Books page route and add cover images in jpg format

---
+++ src/components/Books/index.js
@@ -0,0 +1,14 @@
+import './index.scss'
+import Loader from "react-loaders"
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
+const Books = () => {
+
+    return (
+        <div>Hello</div>
+    )
+}
+
+export default Books
